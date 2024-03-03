fd49c4e0e09e1839e2d82ad4057aa8d02ddf0042
Zhang <panpan.zhang@vumc.org>
2024-03-02 23:28:05 -0600

    edit main R function

--- R/ppcm.R
+++ R/ppcm.R
@@ -1,4 +1,51 @@
-library(dplyr)
+##
+## ppcm: Predictive Partly Conditional Model
+## Copyright (C) 2024 Panpan Zhang and Dandan Liu
+## Panpan Zhang <panpan.zhang@vumc.org>
+##
+## This file is part of the R package nonparlongdat.
+##
+## The R package wdnet is free software: You can redistribute it and/or
+## modify it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or any later
+## version (at your option). See the GNU General Public License at
+## <https://www.gnu.org/licenses/> for details.
+##
+## The R package wdnet is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+##
+
+#' @importFrom dplyr
+#' @importFrom rms lmr
+NULL
+
+##  ppcm: Predictive Partly Conditional Model
+
+#' Predictive Partly Conditional Model
+#' 
+#' This function
+#'
+#' @param data is a data frame in a long format
+#' @param id.var is the variable name indicating ID or similar
+#' @param outcome.var is the variable name of the outcome
+#' @param time.var is the variable name of the follow-up time
+#' @param cov.ti is the list of variable names of the time-independent covariates
+#' @param cov.td is the list of variable names of the time-dependent covariates
+#' @param method allows the users to select between the restricted \code{ppcm} 
+#' using fixed time window only and the unrestricted \code{ppcm-u} using all
+#' available data.
+#' 
+#'
+#' @return a list of parameter estimates and standard errors.
+#'
+#' @references Neal, J. E., Zhang, P. & Liu, D. (2024). Predictive partly conditional
+#' models for longitudinal ordinal outcomes with application to Alzheimer's disease
+#' progression. Under review.
+#'
+#' @keywords internal
+#'
+#' @export
 
 ppcm <- function(data, 
                  id.var, 
